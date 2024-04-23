958fa93030e58975f90ec01772f4ce6784cc7cc5
jungchansoo <awjdckstn97@gmail.com>
2024-04-23 13:58:29 +0900

    백준 2581 해결

--- src/BaekJoon/q_2581.java
+++ src/BaekJoon/q_2581.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class q_2581 {
 	
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
