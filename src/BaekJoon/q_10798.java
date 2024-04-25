77ce99359cee511cbcc63d7053665df7a3f9e7c9
jungchansoo <awjdckstn97@gmail.com>
2024-04-25 20:10:38 +0900

    백준 10798해결

--- src/BaekJoon/q_10798.java
+++ src/BaekJoon/q_10798.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class q_10798 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
